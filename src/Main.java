4b5af030c04357f9f2f9b5a0fe6dbbcbcb160e91
kristynf <thetruthandsoul@gmail.com>
2020-01-27 12:34:29 -0500

    1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main  {
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
 
         int num, total = 0;
