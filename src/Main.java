b7352c5f909e566825f1dc62e3ebe39e391c1182
GBTC441015UR <GBTC441015UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-01-27 13:23:47 -0500

    updated

--- src/Main.java
+++ src/Main.java
@@ -9,17 +9,36 @@ public class Main  {
 
         System.out.println("This is a counting program");
         // Loop should start here
-        for (num = 0; num < 9; num++) {
+        for (int i =0; i < 9; i ++){
             System.out.println("Please enter a number you want to add: ");
             num = keyboard.nextInt();
 
             total += num;
+            keyboard.nextLine();
+
             // Loop should end here
-
-            System.out.println("The total of all 10 numbers is " + total);
-
         }
-
+        System.out.println("The total of all 10 numbers is: " + total);
     }
 }
 // The following program should allow the users to input 10 integers and output the total. Please finish it.
+
+//import java.util.Scanner;
+//
+//public class GetIntegers {
+//
+//    public void main(String[] args) {
+//        Scanner keyboard = new Scanner(System.in);
+//
+//        int num, total = 0;
+//
+//        // Loop should start here
+//        num = keyboard.nextInt();
+//        total += num;
+//        // Loop should end here
+//
+//        System.out.println("The total of all 10 numbers is " + total);
+//
+//    }
+//
+//}
